3f79054db8282718f1f95cd7f7f57c2b14263530
Mauro Zangaro <mzangaro@c2r10s6.42madrid.com>
2025-10-16 15:51:32 +0200

    studying

--- repeated/inter.c
+++ repeated/inter.c
@@ -21,3 +21,48 @@ $>./inter "rien" "cette phrase ne cache rien" | cat -e
 rien$
 $>./inter | cat -e
 $ */
+
+#include <unistd.h>
+
+int	is_repeated(char s2, char *s1, int n)
+{
+	int i = 0;
+	while (i < n)
+	{
+		if (s1[i] == s2)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 3)
+	{
+		char *s1 = argv[1];
+		char *s2 = argv[2];
+		int i = 0;
+		int j = 0;
+
+		while (s1[i])
+		{
+			j = 0;
+			while (s2[j])
+			{
+				if (s1[i] == s2[j])
+				{
+					if (is_repeated(s2[j], s1, i) == 0)
+					{
+						write(1, &s1[i], 1);
+						break;
+					}
+				}
+				j++;
+			}
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
